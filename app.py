721c8ea31766d9f2c33216a89811707edd33e88e
shimseonjo1 <shimseonjo@naver.com>
2022-04-02 11:31:33 +0900

    모델 삭제

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
+from copyreg import pickle
 from flask import Flask,render_template, request, redirect
-import os
+import os,pickle
+from PIL import Image
+import numpy as np
+
 
 app = Flask(__name__)
 
@@ -39,6 +43,27 @@ def upload():
         print(path)
         f.save(path)
         return redirect('/')
+
+@app.route('/mnist',methods=['GET','POST'])
+def mnist():
+    if request.method =='GET':
+        return render_template('mnistform.html')
+    else:
+        f= request.files['filename']
+        path = os.path.dirname(__file__)+'/static/upload/'+ f.filename
+        print(path)
+        f.save(path)
+        img = Image.open(path).convert('L')
+        img = np.resize(img,(1,784))
+        img = 255-(img)
+        path = 'static/upload/'+ f.filename
+        modelpath = 'model.data'
+        f = open(modelpath,'rb') 
+        model = pickle.load(f)
+        pred = model.predict(img)
+        print(pred)
+        return render_template('mnistresult.html',data=[pred[0],path])
+
 
 
 if __name__ == '__main__':
